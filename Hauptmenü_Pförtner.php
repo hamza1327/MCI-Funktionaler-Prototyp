03a2d4a6277ba7bafa26f4960880af56d8800078
hamza_yavas <hamza_bilal.yavas@smail.th-koeln.de>
2020-06-26 11:42:16 +0200

    Hauptmenü_Pförtner.php, hilfe.css für das Fragezeichen mit Kreis

---
+++ Hauptmenü_Pförtner.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- CSS -->
+    <meta charset="utf-8">
+    <link href="css/anmelden.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="css/hilfe.css" rel="stylesheet">
+</head>
+  <body>
+  <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #82187c;">
+      <div class="container-fluid">
+          <div class="navbar-header">
+              <a class="navbar" style="font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: white;" href="link zum HM">zurück zum Hauptmenü</a>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+              <h4 class="navbar-text" style="font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: white; padding-top: 2mm">***user name | time***</h4>
+          </ul>
+      </div>
+      <div class="container-fluid" style="background-color: white;">
+          <h6 class="text-left" style="font-size: 25px; font-family: Arial, Helvetica, sans-serif; color: black;">Transponder / Raum Übersicht</h6>
+      </div>
+  </nav>
+
+
+ /<div class="container-fluid" style="height: 500px">
+<!--        </div>-->
+<!---->
+<!--        <form class = "form-anmelden">-->
+<!--          <h1 class="h3 mb-3 mt-5 font-weight-light" style="text-align: left;">Mit der CampusID anmelden</h1>-->
+<!---->
+<!--          <hr>-->
+<!---->
+<!--          <div class="row">-->
+<!--            <div class="col">-->
+<!--              <p>Benutzername</p>-->
+<!--            </div>-->
+<!--            <div class="col-9">-->
+<!--              <input type="text" name="inputName" class="form-control" required autofocus>-->
+<!--            </div>-->
+<!--          </div>-->
+<!---->
+<!--          <div class="row">-->
+<!--            <div class="col">-->
+<!--              <p>Passwort</p>-->
+<!--            </div>-->
+<!--            <div class="col-9">-->
+<!--              <input type="password" name="inputPassword" class="form-control" required>-->
+<!--            </div>-->
+<!--          </div>-->
+<!---->
+<!--          <hr>-->
+<!---->
+<!--          <div class="row">-->
+<!--            <div style="margin-left: 520px">-->
+<!--              <button class="btn btn-primary" type="submit">Anmelden</button>-->
+<!--            </div>-->
+<!--          </div>-->
+<!---->
+<!--        </form>-->
+    </div>
+
+      <footer class="container-fluid"  style=" background-color: white;
+              color: black; border-top: 1px solid #000000;  ">
+          <div id="box1">
+          <p style=" color: white";> ?</p>
+          </div>
+          <p>Impressum | Datenschutz</p>
+      </footer>
+  </body>
+</html>
