5fcc35fb3bc7652783977c9a0f56a1de9a1d63f6
hamza_yavas <hamza_bilal.yavas@smail.th-koeln.de>
2020-06-29 17:37:14 +0200

    Hauptmenü_Pförtner.php: Verlinkungen

--- pförtner_hauptmenü.php
+++ pförtner_hauptmenü.php
@@ -7,54 +7,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/hilfe.css" rel="stylesheet">
 </head>
-  <body>
-      <nav class="navbar navbar-expand-md fixed-top" style="background-color: #82187c;">
-          <span class="navbar-text w-100 pl-10 text-right order-1 order-md-last" style="font-size: 20px; font-family: Arial, Helvetica, sans-serif; color: white;">Angemeldet als Bolle Berg | 27.06.2020 10:00</span>
-      </nav>
-      <div class="container-fluid" style="background-color: white; margin-top: 60px; border-bottom: 1px solid black">
-          <h6 class="text-left" style="font-size: 30px; font-family: Arial, Helvetica, sans-serif; color: black; padding-left: 15px">Hauptmenü</h6>
-      </div>
-
-    <div class="container-fluid">
-        <div class="row">
-              <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
-                  <h4 style="margin-top: 20px"> Raum / Transponder </h4>
-                  <hr style="width: 100%; border-top: 1px solid #000000;">
-                <div>
-                  <button class="btn btn-primary" type="submit" style="width: 130px; height: 30px;">Übersicht anzeigen</button>
-                </div>
-              </div>
-
-              <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
-                  <h4 style="margin-top: 20px"> Transponder Ausleihe </h4>
-                  <hr style="width: 100%; border-top: 1px solid #000000">
-                  <div>
-                      <button class="btn btn-primary custom-button" type="submit"
-                              style="width: 130px; height: 30px" value="true">Verleihen</button>
-                  </div>
-                  <div style= "margin-top: 10px">
-                      <button class="btn btn-primary custom-button" type="submit"
-                              style="width: 130px; height: 30px" value="true">Erhalten</button>
-                  </div>
-              </div>
-
-              <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
-                  <h4 style="margin-top: 20px"> Offline Modus </h4>
-                  <hr style="width: 100%; border-top: 1px solid #000000">
-                  <div>
-                      <button class="btn btn-primary custom-button" type="submit"
-                              style="width: 130px; height: 30px" value="true">Aktivieren</button>
-                  </div>
-                  <div style= "margin-top: 10px">
-                      <button class="btn btn-primary custom-button" type="submit"
-                              style="width: 130px; height: 30px" value="true">Beenden</button>
-                  </div>
-              </div>
+<body>
+<nav class="navbar navbar-expand-md fixed-top" style="background-color: #82187c;">
+    <span class="navbar-text w-100 pl-10 text-right order-1 order-md-last" style="font-size: 20px; font-family: Arial, Helvetica, sans-serif; color: white;">Angemeldet als Bolle Berg | 27.06.2020 10:00</span>
+</nav>
+<div class="container-fluid" style="background-color: white; margin-top: 60px; border-bottom: 1px solid black">
+    <h6 class="text-left" style="font-size: 30px; font-family: Arial, Helvetica, sans-serif; color: black; padding-left: 15px">Hauptmenü</h6>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
+            <h4 style="margin-top: 20px"> Raum / Transponder </h4>
+            <hr style="width: 100%; border-top: 1px solid #000000;">
+            <div>
+                <button class="btn btn-primary" type="submit" style="width: 130px; height: 30px;"><a href ="pförtner_übersicht.php">Übersicht anzeigen</a></button></div>
+            </div>
+        </div>
+        <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
+            <h4 style="margin-top: 20px"> Transponder Ausleihe </h4>
+            <hr style="width: 100%; border-top: 1px solid #000000">
+            <div>
+                <button class="btn btn-primary custom-button" type="submit"
+                        style="width: 130px; height: 30px" value="true" ><a href ="pförtner_ausleihe.php">Verleihen</a></button>
+            </div>
+            <div style= "margin-top: 10px">
+                <button class="btn btn-primary custom-button" type="submit"
+                        style="width: 130px; height: 30px" value="true">Erhalten</button>
+            </div><a href ="pförtner_übersicht.php">Übersicht anzeigen</a>
+        </div>
+        <div class="col-sm-2 text-center" style=" border-right: 1px solid black; height: 540px">
+            <h4 style="margin-top: 20px"> Offline Modus </h4>
+            <hr style="width: 100%; border-top: 1px solid #000000">
+            <div>
+                <button class="btn btn-primary custom-button" type="submit"
+                        style="width: 130px; height: 30px" value="true">Aktivieren</button>
+            </div>
+            <div style= "margin-top: 10px">
+                <button class="btn btn-primary custom-button" type="submit"
+                        style="width: 130px; height: 30px" value="true" >Beenden</button>
+            </div>
         </div>
     </div>
-
-      <hr style="width: 100%; bottom: 43px; position: fixed; border-top: 1px solid #000000">
-      <p style="bottom: 5px; left: 20px; position: fixed;">Impressum | Datenschutz</p>
-      <img src="assets/frage.png" alt="" width="50px" height="50px" style="right: 41px; bottom: 36px; position: fixed;">
-  </body>
+</div>
+<hr style="width: 100%; bottom: 43px; position: fixed; border-top: 1px solid #000000">
+<p style="bottom: 5px; left: 20px; position: fixed;">Impressum | Datenschutz</p>
+<img src="assets/frage.png" alt="" width="50px" height="50px" style="right: 41px; bottom: 36px; position: fixed;">
+</body>
 </html>
